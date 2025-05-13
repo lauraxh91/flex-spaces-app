5c550ede3515054b0784907658f6deb1bbe035af
Laura Xhaferaj <lauretaxh@gmail.com>
2025-05-13 16:01:40 +0200

    used uri

--- api/submit.js
+++ api/submit.js
@@ -11,7 +11,7 @@ if (!cached) {
 async function connectToDatabase() {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    cached.promise = mongoose.connect(uri, {
+    cached.promise = mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }).then((mongoose) => mongoose);
