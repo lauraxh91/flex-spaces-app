df85ffff41ed730c76c3e66f933348b911077310
Laura Xhaferaj <lauretaxh@gmail.com>
2025-05-18 21:06:31 +0200

    removed typo

--- src/components/Text/CallOut.jsx
+++ src/components/Text/CallOut.jsx
@@ -28,7 +28,7 @@ const styles = {
 
 const CallOut = ({
   title = 'More People Work Remotely Than Ever — Attract Them to Your Space',
-  description = `Remote professionals are working differently. They seek freedom, not long-term contracts, but day passes, week passes, and drop-ins. By listing your space on our platform, you can fill unused desks and bring in additional revenue from ad-hoc users, without disrupting your existing members. Be among the first featured spaces when we launch.`,
+  description = `Remote professionals are working differently. They seek freedom, not long-term contracts, but day passes, week passes, and drop-ins. By listing your space on our platform, you can fill unused desks and bring in additional revenue from ad-hoc users, without disrupting your existing members. Be among the first featured spaces when we launch.,
 }) => {
   return (
     <div style={styles.cardWrapper}>
