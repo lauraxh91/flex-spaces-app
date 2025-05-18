89800714133decc86e243ddde760156fd954930a
Laura Xhaferaj <lauretaxh@gmail.com>
2025-05-18 18:53:38 +0200

    changed ,,

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ✅ ensures correct relative paths in build
   server: {
     proxy: {
       '/api': {
